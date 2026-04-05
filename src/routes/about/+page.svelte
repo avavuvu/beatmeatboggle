7caa5452ef61bd3fc53f38470055c3a27d28a008
Ava Vu <avadinhvu@gmail.com>
2026-04-05 22:40:03 +1000

    switched to static adapter and fixed build errors

---
+++ src/routes/about/+page.svelte
@@ -0,0 +1,7 @@
+<div class="text-foreground max-w-4xl mx-auto px-4">
+    <h1 class="px-2">About</h1>
+
+    <main class="border p-4 *>py-2">
+        <p>There's lots of interesting things about me.</p>
+    </main>
+</div>
